77b5a84f668d41fa7d6cc117e03c8ceee16a512f
Adeel AHMED <adeel.ahmed@cgi.com>
2021-02-26 00:46:09 +0100

    Added implementations for Categorie Repo

--- jpa-stream/src/main/java/fr/training/SimpleJpaApp.java
+++ jpa-stream/src/main/java/fr/training/SimpleJpaApp.java
@@ -4,6 +4,11 @@ public class SimpleJpaApp {
 
 	public static void main(String[] args) {
 		System.out.println("Coucou les filles");
+		CategorieService cs = new CategorieService();
+		
+		CategorieEntity c1 = new CategorieEntity("CategorieParent");
+		cs.save(c1);
+		cs.findAll().forEach(System.out::println);
 	}
 
 }
--- jpa-stream/src/main/java/fr/training/dao/jpa/CategorieDaoJpaImpl.java
+++ jpa-stream/src/main/java/fr/training/dao/jpa/CategorieDaoJpaImpl.java
@@ -1,39 +1,90 @@
 package fr.training.dao.jpa;
 
+import static java.util.Collections.*;
+
 import java.util.List;
 
 import fr.training.dao.CategorieDao;
 import fr.training.domain.CategorieEntity;
 
 public class CategorieDaoJpaImpl extends DaoJpa implements CategorieDao {
+	
+	private static final String SELECT_ALL = "SELECT c FROM Categorie c";
+	
+	private static final String SELECT_BY_ID = "SELECT c FROM Categorie c WHERE c.id = :id";
+	
+	private static final String DELETE = "DELETE FROM Categorie c WHERE c.id = :id";
 
 	@Override
 	public List<CategorieEntity> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return em
+					.createQuery(SELECT_ALL, CategorieEntity.class)
+					.getResultList();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return emptyList();
 	}
 
 	@Override
 	public CategorieEntity findById(int id) {
-		// TODO Auto-generated method stub
+		try {
+			return em
+					.createQuery(SELECT_BY_ID, CategorieEntity.class)
+					.setParameter("id", id)
+					.getSingleResult();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
 	@Override
 	public CategorieEntity add(CategorieEntity entity) {
-		// TODO Auto-generated method stub
+		try {
+			em.getTransaction().begin();
+			em.persist(entity);
+			em.getTransaction().commit();
+			return entity;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
 	@Override
 	public CategorieEntity save(CategorieEntity entity) {
-		// TODO Auto-generated method stub
+		try {
+			em.getTransaction().begin();
+			if (entity.getId() > 0) {
+				// Si l'entity a un id, on fait un update
+				entity = em.merge(entity);
+			} else {
+				// Sinon on fait un insert
+				em.persist(entity);
+			}
+			em.getTransaction().commit();
+			return entity;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
 	@Override
 	public boolean deleteById(int id) {
-		// TODO Auto-generated method stub
+		try {
+			em.getTransaction().begin();
+			em
+				.createQuery(DELETE)
+				.setParameter("id", id)
+				.executeUpdate();
+			em.getTransaction().commit();
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return false;
 	}
 

906f5f4e618b9eab22d085ba22107f018584e079
chihab <c.berkane@hotmail.fr>
2021-02-27 23:55:01 +0100

    corrected dao provider

--- jpa-stream/src/main/java/fr/training/dao/DaoProvider.java
+++ jpa-stream/src/main/java/fr/training/dao/DaoProvider.java
@@ -3,6 +3,7 @@ package fr.training.dao;
 import fr.training.dao.jpa.ArticleDaoJpaImpl;
 import fr.training.dao.jpa.CategorieDaoJpaImpl;
 import fr.training.dao.jpa.DresseurDaoJpaImpl;
+import fr.training.dao.jpa.PokemonDaoJpaImpl;
 
 public class DaoProvider {
 	
@@ -16,5 +17,9 @@ public class DaoProvider {
 	
 	public static DresseurDaoJpaImpl getDresseurDao() {
 		return new DresseurDaoJpaImpl();
+	}
+	
+	public static PokemonDaoJpaImpl getPokemonDao() {
+		return new PokemonDaoJpaImpl();
 	}
 }

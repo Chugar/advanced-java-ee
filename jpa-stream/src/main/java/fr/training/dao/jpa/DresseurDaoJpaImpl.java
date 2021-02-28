05b6d17ff1698b97a61b51e8fc4e17ff2152b3f3
Adeel AHMED <adeel.ahmed@cgi.com>
2021-02-28 16:33:31 +0100

    Completed DresseurRepo implementation

--- jpa-stream/src/main/java/fr/training/dao/jpa/DresseurDaoJpaImpl.java
+++ jpa-stream/src/main/java/fr/training/dao/jpa/DresseurDaoJpaImpl.java
@@ -1,49 +1,91 @@
 package fr.training.dao.jpa;
 
-import java.util.List;
+import static java.util.Collections.emptyList;
 
-import javax.persistence.RollbackException;
+import java.util.List;
 
 import fr.training.dao.DresseurDao;
 import fr.training.domain.DresseurEntity;
 
 public class DresseurDaoJpaImpl extends DaoJpa implements DresseurDao {
+	
+	private static final String SELECT_ALL = "SELECT d FROM Dresseur d";
 
+	private static final String SELECT_BY_ID = "SELECT d FROM Dresseur d WHERE d.id = :id";
+	
+	private static final String DELETE = "DELETE FROM Dresseur d WHERE d.id = :id";
+	
 	@Override
 	public List<DresseurEntity> findAll() {
-		return null;
+		try {
+			return em
+					.createQuery(SELECT_ALL, DresseurEntity.class)
+					.getResultList();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return emptyList();
 	}
 
 	@Override
 	public DresseurEntity findById(int id) {
+		try {
+			return em
+					.createQuery(SELECT_BY_ID, DresseurEntity.class)
+					.setParameter("id", id)
+					.getSingleResult();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
 	@Override
 	public DresseurEntity add(DresseurEntity entity) {
+		try {
+			em.getTransaction().begin();
+			em.persist(entity);
+			em.getTransaction().commit();
+			return entity;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
 	@Override
 	public DresseurEntity save(DresseurEntity entity) {
-		
-		DresseurEntity dresseur;
 		try {
+			em.getTransaction().begin();
+			if(entity.getId() > 0) {
+				// UPDATE
+				entity = em.merge(entity);
+			} else {
+				// INSERT
+				em.persist(entity);
+			}
+			em.getTransaction().commit();
+			return entity;
 			
-			this.em.getTransaction().begin();
-			dresseur = this.em.merge(entity);
-			this.em.getTransaction().commit();
-			return dresseur;
-			
-		} catch (RollbackException e) {
-			
+		} catch (Exception e) {
 			e.printStackTrace();
-			return null;
 		}
+		return null;
 	}
 
 	@Override
 	public boolean deleteById(int id) {
+		try {
+			em.getTransaction().begin();
+			em
+				.createQuery(DELETE)
+				.setParameter("id", id)
+				.executeUpdate();
+			em.getTransaction().commit();
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return false;
 	}
 
